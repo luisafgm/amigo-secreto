a4ce4e555ae2427a1d2fd40e4c92636562cb0ead
luisafgm <luisafgm1122@gmail.com>
2022-12-23 23:18:57 +0100

    -n

--- js/functions.js
+++ js/functions.js
@@ -19,57 +19,56 @@ function readTextFile(file, callback) {
 
 //crear contenedor de bolitas con las bolitas
 
-const  bolasParaSorteo=
-["Cambia con quien esta a tú derecha.",
-"Cambia con quien esta a tú izquierda.",
-"Cambia con el que este al frente.",
-"Todos cambian con quien esta a tú derecha.",
-"Todos cambian con quien esta a tú izquierda.",
-"Todos cambian con el que este al frente.",
-"Cambiar con quien tú quieras.",
-"Cambiar con quien tú quieras.",
-"Cambiar con quien tú quieras.",
-"Cambiar con quien tú quieras.",
-"Re ordenar los regalos.",
-"Todos cambian con quien quieran.",
-"No pasa nada.",
-"No pasa nada.",
-"No pasa nada.",
-"Tú y tú pareja reorganizan.",
-"Todos toman un shot.",
-"El de tú derecha toma shot.",
-"Elije quien toma un shot.",
-"Tú y tú pareja cambian.",
-"Tú y tú pareja toman Shot.",
-"Elige que pareja cambia.",
-"Tú tomas shot.",
-"Tú tomas shot.",
-"Tú tomas shot.",
-"Tú tomas shot.",
-"Tú y tú pareja toman Shot.",
-"Tú y tú pareja toman Shot.",
-"Tú pareja toman Shot.",
-"Tú pareja toman Shot.",
+const bolasParaSorteo = ["Cambia con quien esta a tú derecha.",
+    "Cambia con quien esta a tú izquierda.",
+    "Cambia con el que este al frente.",
+    "Todos cambian con quien esta a tú derecha.",
+    "Todos cambian con quien esta a tú izquierda.",
+    "Todos cambian con el que este al frente.",
+    "Cambiar con quien tú quieras.",
+    "Cambiar con quien tú quieras.",
+    "Cambiar con quien tú quieras.",
+    "Cambiar con quien tú quieras.",
+    "Re ordenar los regalos.",
+    "Todos cambian con quien quieran.",
+    "No pasa nada.",
+    "No pasa nada.",
+    "No pasa nada.",
+    "Tú y tú pareja reorganizan.",
+    "Todos toman un shot.",
+    "El de tú derecha toma shot.",
+    "Elije quien toma un shot.",
+    "Tú y tú pareja cambian.",
+    "Tú y tú pareja toman Shot.",
+    "Elige que pareja cambia.",
+    "Tú tomas shot.",
+    "Tú tomas shot.",
+    "Tú tomas shot.",
+    "Tú tomas shot.",
+    "Tú y tú pareja toman Shot.",
+    "Tú y tú pareja toman Shot.",
+    "Tú pareja toma Shot.",
+    "Tú pareja toma Shot.",
 
 ]
 
 //crear contenedor de bolitas que ya salieron 
 
-const  bolasSorteadas=[]
+const bolasSorteadas = []
 
 //mostrar las bolitas
 
-const mostrarBolita = function () {
-    //sacar la primera blita de la lista
-    const bolaEnJuego = agarraLaBolita()
-    //mostrar la bolita
-    document.getElementById("topic").innerHTML = bolaEnJuego
-    //mandar la bola sorteada a las que ya salieron
-    bolasSorteadas.push(bolaEnJuego)
-}
-//saca aleatoriamente la bolita desde las bolas disponibles para el sorteo
-function agarraLaBolita( ) {
+const mostrarBolita = function() {
+        //sacar la primera blita de la lista
+        const bolaEnJuego = agarraLaBolita()
+            //mostrar la bolita
+        document.getElementById("topic").innerHTML = bolaEnJuego
+            //mandar la bola sorteada a las que ya salieron
+        bolasSorteadas.push(bolaEnJuego)
+    }
+    //saca aleatoriamente la bolita desde las bolas disponibles para el sorteo
+function agarraLaBolita() {
     const cantidadDisponible = bolasParaSorteo.length
     const indiceAleatorio = Math.random() * (cantidadDisponible - 0) + 0;
-    return bolasParaSorteo.splice(indiceAleatorio,1)[0]
-} 
+    return bolasParaSorteo.splice(indiceAleatorio, 1)[0]
+}
